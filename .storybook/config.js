b68474214ba94cc3fcc1997ca72a8e754391e3ba
ckinmind <245160421@qq.com>
2018-12-08 00:41:03 +0800

    add addon-info

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,18 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator} from '@storybook/react';
+import {withInfo} from '@storybook/addon-info'
 
 function loadStories() {
   require('../src/stories');
 }
 
+addDecorator(
+  withInfo({
+
+    header: false, // 展示info的头部
+    source: false, // 是否展示源码
+    inline: true, // 是点击切换展示info还是直接显示info
+  })
+);
+
 configure(loadStories, module);
+
--- src/stories/index.js
+++ src/stories/index.js
@@ -6,12 +6,29 @@ import { linkTo } from '@storybook/addon-links';
 
 import { Button, Welcome } from '@storybook/react/demo';
 
+
+storiesOf('Button2', module)
+  .add('Button Component',
+    () =>  <div />,
+    {info: {
+        text: `
+              description or documentation about my component, supports markdown
+    
+              ~~~js
+              <Button>Click Here</Button>
+              ~~~
+            `,
+      },
+    }
+  );
+
 storiesOf('Mobx Beyond', module)
   .add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
 storiesOf('Decorator', module)
-  .add('1. 装饰者模式', () => '')
-  .add('2. 装饰者模式', () => '')
+  .add('1. 装饰者模式', () => 'fdf')
+  .add('2. Decorators in ES7', () => 'ffdf')
+  .add('3. Decorators使用场景和案例', () => 'ffdf')
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
